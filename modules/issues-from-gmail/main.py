133c2247da679c8e7d103867ef62a44cb8c2f8b9
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-07-03 11:38:53 -0600

    Connect to Gmail over IMAP, figure out how to track things (using labels)

--- modules/issues-from-gmail/main.py
+++ modules/issues-from-gmail/main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
-# https://github.com/The-Yak-Collective/gmail_hook/blob/59ddee2679800d85edf9719259fb7bb329cb8eae/gmailcalv1.py
-# https://docs.github.com/en/rest/issues
-# https://stackoverflow.com/a/70929145
+# https://docs.python.org/3/library/imaplib.html
+# https://developers.google.com/gmail/imap/imap-extensions
+# https://support.google.com/mail/answer/7190?hl=en
+
+# Import necessary packages
+#
+import imaplib
+import os
+
+# Grab login information from the environment
+#
+gmail_user = os.environ['GMAIL_USER']
+gmail_pass = os.environ['GMAIL_PASS']
+
+# Connect to Gmail
+#
+server = imaplib.IMAP4_SSL("imap.gmail.com")
+server.login(gmail_user, gmail_pass)
+server.select('"' + "INBOX" + '"')
+# server.search(None, 'X-GM-RAW "to:(yakcollective.org+atest@gmail.com) -label:(lunchtime-tickets)"')
+# server.store("2071", "+X-GM-LABELS", "(lunchtime-tickets)")
+server.logout()
