1d740aef83b9d579ddab3f2651ce9c4cb9e476de
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-08-21 17:56:22 -0600

    Prevent fetching headers when there are no messages to process

--- modules/issues-from-gmail/main.py
+++ modules/issues-from-gmail/main.py
@@ -45,88 +45,94 @@ response, raw_unprocessed_message_set = server.search(None, 'X-GM-RAW "to:(' + g
 #
 unprocessed_message_set_string = ",".join(raw_unprocessed_message_set[0].decode().split())
 
-# Fetch unprocessed message "To" headers
+# If len(unprocessed_message_set_string) == 0, then there are no new
+# messages and we can just bail.
 #
-response, raw_ToField_headers = server.fetch(unprocessed_message_set_string, "(BODY[HEADER.FIELDS (TO)])")
+if len(unprocessed_message_set_string) == 0:
 
-# Loop over raw headers and extract message number and "To" address.
-#
-# If "To" address contains a "+", then fetch the full ("RFC822")
-# message and create a corresponding issue in GitHub.
-#
-# Regardless of whether the message was turned into an issue, mark it
-# as processed so that we don't have to read it again.
-#
-for raw_ToField_header in raw_ToField_headers:
-	if isinstance(raw_ToField_header, tuple):
-		message_number = raw_ToField_header[0].decode().split()[0]
+	# Fetch unprocessed message "To" headers
+	#
+	response, raw_ToField_headers = server.fetch(unprocessed_message_set_string, "(BODY[HEADER.FIELDS (TO)])")
 
-		# TODO - This will still match to strings like
-		# "yakcollective.org+foo@gmail.com <evil.email@example.com>".
-		# If we wanted to do this right, we'd create a list of all
-		# "To" emails, parse out the *actual* email address (ignoring
-		# the "friendly name"), and *then* match.
-		#
-		message_match = re.search(r'[\s,<]' + gmail_user_part + '\+[^@]+\@' + gmail_domain_part + '[>,\s]', raw_ToField_header[1].decode())
+	# Loop over raw headers and extract message number and "To"
+	# address.
+	#
+	# If "To" address contains a "+", then fetch the full ("RFC822")
+	# message and create a corresponding issue in GitHub.
+	#
+	# Regardless of whether the message was turned into an issue, mark
+	# it as processed so that we don't have to read it again.
+	#
+	for raw_ToField_header in raw_ToField_headers:
+		if isinstance(raw_ToField_header, tuple):
+			message_number = raw_ToField_header[0].decode().split()[0]
 
-		if message_match:
-			response, raw_message = server.fetch(message_number, "(RFC822)")
-			message = email.message_from_string(raw_message[0][1].decode(), policy=policy.default)
-
-			# Walk through message parts and extract MIME parts.
-			# 
-			# Parts that are text/plain or text/html without a content
-			# disposition are treated as the message body; if multiple
-			# such parts of the same type are found, we concatenate
-			# them. Parts without a content disposition that are *not*
-			# of type text/plain or text/html are ignored.
+			# TODO - This will still match to strings like
+			# "yakcollective.org+foo@gmail.com <evil.email@example.com>".
+			# If we wanted to do this right, we'd create a list of all
+			# "To" emails, parse out the *actual* email address
+			# (ignoring the "friendly name"), and *then* match.
 			#
-			# Parts that *have* a content disposition and a file name
-			# are treated as attachments. Parts with a content
-			# disposition and no file name are ignored.
-			#
-			# TODO - Ideally, we should separately track content that
-			# has a content disposition of "inline" and then try to
-			# re-embed it when creating the issue in GitHub.
-			#
-			message_body = {
-				"text": "",
-				"html": ""
-			}
+			message_match = re.search(r'[\s,<]' + gmail_user_part + '\+[^@]+\@' + gmail_domain_part + '[>,\s]', raw_ToField_header[1].decode())
 
-			for message_part in message.walk():
-				if not message_part.get_content_type().startswith("multipart/"):
-					if not message_part.get_content_disposition() and (message_part.get_content_type() == "text/plain" or message_part.get_content_type() == "text/html"):
-						if message_part.get_content_type() == "text/plain":
-							message_body["text"] = (message_body["text"] + "\n\n" + message_part.get_content().strip()).strip()
-						elif message_part.get_content_type() == "text/html":
-							message_body["html"] = (message_body["html"] + message_part.get_content().strip()).strip()
+			if message_match:
+				response, raw_message = server.fetch(message_number, "(RFC822)")
+				message = email.message_from_string(raw_message[0][1].decode(), policy=policy.default)
 
-			# It's very rare these days that much care is taken with
-			# the plain text part of messages. If an HTML part exists,
-			# we will almost always get a nicer issue by converting
-			# this into Markdown than by preferring the plain text
-			# part.
-			#
-			# We do a quick-and-dirty replacement to turn
-			# <div> and </div> tags into <p> and </p> tags, as a lot of
-			# email clients like to wrap things in <div> tags these
-			# days, rather than using <p> tags.
-			#
-			# We also need to deal with the <div>-<br>-<div> sequence
-			# without busting the ability to use <br> tags in general.
-			# Hence the wacky replacement after conversion.
-			#
-			if message_body["html"] != "":
-				message_body["text"] = md(message_body["html"].replace("<div ", "<p ").replace("<div>", "<p>").replace("</div>", "</p>"), heading_style = "ATX").replace("\n\n  \n\n\n", "\n\n").strip()
+				# Walk through message parts and extract the message
+				# contents.
+				# 
+				# Parts that are text/plain or text/html without a
+				# content disposition are treated as the message body;
+				# if multiple such parts of the same type are found, we
+				# concatenate them. Parts without a content disposition
+				# that are *not* of type text/plain or text/html are
+				# ignored.
+				#
+				# Parts that *have* a content disposition and a file
+				# name are (normally) attachments. We don't deal with
+				# these here, as the GitHub API doesn't allow us to add
+				# attachments to issues right now.
+				#
+				message_body = {
+					"text": "",
+					"html": ""
+				}
 
-			# Create ticket
-			#
-			lunchtime_tickets.create_issue(title = message["Subject"], body = message_body["text"])
+				for message_part in message.walk():
+					if not message_part.get_content_type().startswith("multipart/"):
+						if not message_part.get_content_disposition() and (message_part.get_content_type() == "text/plain" or message_part.get_content_type() == "text/html"):
+							if message_part.get_content_type() == "text/plain":
+								message_body["text"] = (message_body["text"] + "\n\n" + message_part.get_content().strip()).strip()
+							elif message_part.get_content_type() == "text/html":
+								message_body["html"] = (message_body["html"] + message_part.get_content().strip()).strip()
 
-		# Mark message as processed
-		#
-		server.store(message_number, "+X-GM-LABELS", "(processed-by-lunchtime-tickets)")		
+				# It's very rare these days that much care is taken
+				# with the plain text part of messages. If an HTML part
+				# exists, we will almost always get a nicer issue by
+				# converting this into Markdown than by preferring the
+				# plain text part.
+				#
+				# We do a quick-and-dirty replacement to turn <div> and
+				# </div> tags into <p> and </p> tags, as a lot of email
+				# clients like to wrap things in <div> tags these days,
+				# rather than using <p> tags.
+				#
+				# We also need to deal with the <div>-<br>-<div>
+				# sequence without busting the ability to use <br> tags
+				# in general. Hence the wacky replacement *after*
+				# conversion.
+				#
+				if message_body["html"] != "":
+					message_body["text"] = md(message_body["html"].replace("<div ", "<p ").replace("<div>", "<p>").replace("</div>", "</p>"), heading_style = "ATX").replace("\n\n  \n\n\n", "\n\n").strip()
+
+				# Create ticket
+				#
+				lunchtime_tickets.create_issue(title = message["Subject"], body = message_body["text"])
+
+			# Mark message as processed
+			#
+			server.store(message_number, "+X-GM-LABELS", "(processed-by-lunchtime-tickets)")		
 
 # Be kind, rewind
 #
