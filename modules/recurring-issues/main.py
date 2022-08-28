f0649e7304b6640b7872f91724e5e2d4da292de7
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-08-28 10:50:00 -0600

    Initial pass at recurring issues

--- modules/recurring-issues/main.py
+++ modules/recurring-issues/main.py
@@ -1,3 +1,50 @@
 #!/usr/bin/env python3
 
-# https://docs.github.com/en/rest/issues
+# Import necessary packages
+#
+import os
+
+from datetime import datetime, timedelta
+from github import Github
+
+# Grab login information from the environment
+#
+github_token = os.environ["ACCESS_TOKEN"]
+github_repo = os.environ["GITHUB_REPOSITORY"]
+
+# Connect to GitHub
+#
+gh = Github(github_token)
+lunchtime_tickets = gh.get_repo(github_repo)
+
+# Pull all of our closed issues
+#
+closed_issues = lunchtime_tickets.get_issues(state='closed')
+
+# Loop over closed issues looking for recurring issues
+#
+for issue in closed_issues:
+	if issue.title.startswith("[RECURRING]"):
+		try:
+			# Extract frequency/current/next
+			#
+			body_array = issue.body.split("\r\n",3)
+			frequency = int(body_array[0].split(": ")[1])
+			current = datetime.strptime(body_array[1].split(": ")[1], "%Y-%m-%d")
+			next = datetime.strptime(body_array[2].split(": ")[1], "%Y-%m-%d")
+
+			# Only continue processing if `next` <= today
+			#
+			if next <= datetime.today():
+				current = datetime.today()
+				next = datetime.today() + timedelta(days = frequency)
+
+				issue_body = "frequency: " + str(frequency) + "\r\n"
+				issue_body += "current: " + current.strftime("%Y-%m-%d") + "\r\n"
+				issue_body += "next: " + next.strftime("%Y-%m-%d") + "\r\n" 
+				issue_body += body_array[3]
+
+				issue.edit(body = issue_body, state = "open")
+
+		except:
+			lunchtime_tickets.create_issue(title = "Failed to process recurring issue #" + str(issue.number), body = "[" + issue.title + "](" +  issue.url + ")")
